6c96c3bdcde404235c73c3103f0d547aa3129309
not-a-mad-sayantist <rockistuart@gmail.com>
2026-01-05 23:40:47 +0530

    Refactor Program.cs to streamline service registration and application configuration

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,72 +1,17 @@
-﻿using ContosoUniversity.Data;
-using ContosoUniversity.Infrastructure;
-using ContosoUniversity.Infrastructure.Tags;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using HtmlTags;
-using MediatR;
+﻿using ContosoUniversity.Infrastructure;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-RegisterServices(builder);
+builder.Services.AddContosoServices(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-ConfigureApplication(app);
+app.ConfigurePipeline();
 
 app.Run();
-
-static void RegisterServices(WebApplicationBuilder builder)
-{
-    var services = builder.Services;
-
-    services.AddMiniProfiler().AddEntityFramework();
-
-    services.AddDbContext<SchoolContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("db")));
-
-    services.AddAutoMapper(_ => { }, typeof(Program));
-
-    services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
-
-    services.AddHtmlTags(new TagConventions());
-
-    services.AddRazorPages(opt =>
-        {
-            opt.Conventions.ConfigureFilter(new DbContextTransactionPageFilter());
-            opt.Conventions.ConfigureFilter(new ValidatorPageFilter());
-        });
-
-    services.AddFluentValidationAutoValidation();
-    services.AddFluentValidationClientsideAdapters();
-    services.AddValidatorsFromAssemblyContaining<Program>();
-
-    services.AddMvc(opt => opt.ModelBinderProviders.Insert(0, new EntityModelBinderProvider()));
-}
-
-static void ConfigureApplication(WebApplication app)
-{
-    app.UseMiniProfiler();
-
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Error");
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseAuthorization();
-
-    app.MapRazorPages();
-}
